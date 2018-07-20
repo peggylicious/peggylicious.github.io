48f18caf4d70f2a9f5d79c5c229bbfe96b592875
peggylicious <peggylicious>
2018-07-20 05:30:09 +0100

    Commentd out the myFunction() function in the javascript file to correct the apperance of a big gytter in the .second-hero section

--- peggy-responsive/script.js
+++ peggy-responsive/script.js
@@ -3,19 +3,22 @@ window.onscroll = function(){
     transitionElement()
 }
 
-    // Adds fixed header to header
-function myFunction(){
-    let nav = document.getElementById("fix-header");
-        // Gets the heigth of the header
-    let sticky = nav.offsetTop;
     
-    if (window.pageYOffset > sticky){
-        nav.classList.add("sticky");
-    }else{
-        nav.classList.remove("sticky");
-    }
-}
+// function myFunction(){
+//     let nav = document.getElementById("fix-header");
+        
+//     let sticky = nav.offsetTop;
+    
+//     if (window.pageYOffset > sticky){
+//         nav.classList.add("sticky");
+//     }else{
+//         nav.classList.remove("sticky");
+//     }
+// }
 
+
+// Gets the heigth of the header
+// Adds fixed header to header
 
 
     // Adds transition to .row-1 of the stories class
