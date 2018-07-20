e15c94efaff95b7a4479c2d7c387c01a3891fa47
peggylicious <peggylicious>
2018-07-20 05:35:17 +0100

    undo changes to both functions

--- peggy-responsive/script.js
+++ peggy-responsive/script.js
@@ -3,37 +3,33 @@ window.onscroll = function(){
     transitionElement()
 }
 
+    // Adds fixed header to header
+function myFunction(){
+    let nav = document.getElementById("fix-header");
+        // Gets the heigth of the header
+    let sticky = nav.offsetTop;
     
-// function myFunction(){
-//     let nav = document.getElementById("fix-header");
-        
-//     let sticky = nav.offsetTop;
-    
-//     if (window.pageYOffset > sticky){
-//         nav.classList.add("sticky");
-//     }else{
-//         nav.classList.remove("sticky");
-//     }
-// }
+    if (window.pageYOffset > sticky){
+        nav.classList.add("sticky");
+    }else{
+        nav.classList.remove("sticky");
+    }
+}
 
-
-// Gets the heigth of the header
-// Adds fixed header to header
 
 
     // Adds transition to .row-1 of the stories class
-            // let desiredPosition = 500;
+function transitionElement(){
+        // let desiredPosition = 500;
+    let affectedElement = document.getElementsByClassName("row-1");
+    let element = document.getElementById("image-container");
+    let imageContainer = element.offsetHeight;
 
-// function transitionElement(){
-//     let affectedElement = document.getElementsByClassName("row-1");
-//     let element = document.getElementById("image-container");
-//     let imageContainer = element.offsetHeight;
-
-//     if (window.pageYOffset >= imageContainer){
-//         affectedElement[0].classList.add('transit');  
-//     }else{
-//         affectedElement[0].classList.remove('transit');
-//     }
+    if (window.pageYOffset >= imageContainer){
+        affectedElement[0].classList.add('transit');  
+    }else{
+        affectedElement[0].classList.remove('transit');
+    }
 }
 
 
